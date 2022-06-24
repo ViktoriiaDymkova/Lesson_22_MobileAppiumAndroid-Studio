3a14a89a2f08eb2f414578244d3213fa6e71680f
user <stel-vd@yandex.ru>
2022-06-24 17:41:17 +0300

    Added DriverSettings

--- src/test/java/tests/local/SearchTests.java
+++ src/test/java/tests/local/SearchTests.java
@@ -3,12 +3,15 @@ package tests.local;
 
 import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class SearchTests extends TestBase {
+
+    @Tag("android")
     @Test
     void searchTest() {
         back();
